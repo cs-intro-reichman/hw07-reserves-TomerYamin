af79eb3416d3c47e4beab5e691a8fa448140d72b
Tomer Yamin <tomery@vintage-ip.com>
2024-03-02 10:19:12 +0200

    HW7

--- SpellChecker.java
+++ SpellChecker.java
@@ -8,28 +8,86 @@ public class SpellChecker {
 		String[] dictionary = readDictionary("dictionary.txt");
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
+		
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		if (str.length() == 1) {
+			return "";
+		}
+
+		else{
+			return str.substring(1);
+		}
+
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+		int lengthWord1 = word1.length();
+		int lengthWord2 = word2.length();
+
+		int insert = 0;
+		int delete = 0;
+		int replace = 0;
+
+
+		if (lengthWord1 == 0) {
+			return lengthWord2;
+		}
+
+		if (lengthWord2 == 0) {
+			return lengthWord1;
+		}
+
+		else if (word1.charAt(0) == word2.charAt(0)){
+			return levenshtein(tail(word1), tail(word2));
+		}
+
+
+		else {
+
+			insert = levenshtein(tail(word1), word2);
+			delete = levenshtein(word1, tail(word2));
+			replace = levenshtein(tail(word1), tail(word2));
+
+			int minTemp = Math.min(insert, delete);
+			int min = Math.min(minTemp, replace);
+			
+			return 1 + min;
+		}
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
 
-		// Your code here
+		for (int i =0; i < dictionary.length; i++) {
+			dictionary[i] = in.readString();
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		int minDistance = levenshtein(word, dictionary[0]);
+		String wordMinDistance = "" ;
+
+		for (int i = 0; i < dictionary.length; i++) {
+			if (levenshtein(word, dictionary[i]) < minDistance ) {
+				wordMinDistance = dictionary[i];
+				minDistance = levenshtein(word, dictionary[i]);
+			}
+		}
+			
+		if (minDistance > threshold) {
+			return word;
+		}	
+
+		else {
+			return wordMinDistance;
+		}
 	}
 
 }
